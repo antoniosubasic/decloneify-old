b9bdfb0bf9c5aa619733ed8eb4e744d598baf70a
Antonio Subašić <antonio.subasic.public@gmail.com>
2024-09-13 14:59:28 +0200

    refactored main.rs
    improved code with better error-handling and solved edge-cases
    beautified console logs

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,83 @@
 mod spotify;
 mod trackmap;
 
+use colored::Colorize;
 use dotenv::dotenv;
-use std::{collections::HashMap, env, fs};
+use std::{
+    collections::HashMap,
+    env,
+    fs::{self, OpenOptions},
+    io::Write,
+    process,
+};
 use trackmap::Trackmap;
+
+fn write_to_env_file(key: &str, value: &str) {
+    let mut file = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open(".env")
+        .expect("failed to open .env");
+
+    writeln!(file, "{}={}", key, value).expect("failed to write to .env");
+}
+
+fn get_env_var(key: &str) -> String {
+    match env::var(key) {
+        Ok(val) => val,
+        Err(_) => {
+            print!("enter your {}: ", key);
+            let mut val = String::new();
+            std::io::stdin().read_line(&mut val).unwrap();
+            let val = val.trim().to_string();
+
+            write_to_env_file(key, &val);
+
+            val
+        }
+    }
+}
+
+fn throw_error(e: String) {
+    eprintln!("{}", e.red());
+    process::exit(1);
+}
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
 
-    let client_id = env::var("CLIENT_ID").expect("CLIENT_ID must be set");
-    let client_secret = env::var("CLIENT_SECRET").expect("CLIENT_SECRET must be set");
-    let user_id = env::var("USER_ID").expect("USER_ID must be set");
+    let client_id = get_env_var("CLIENT_ID");
+    let client_secret = get_env_var("CLIENT_SECRET");
+    let user_id = get_env_var("USER_ID");
 
-    let trackmap_content = fs::read_to_string("trackmap.toml").unwrap_or(String::new());
-    let trackmap: Trackmap = toml::from_str(&trackmap_content).expect("invalid trackmap.toml");
+    let trackmap: Trackmap =
+        toml::from_str(&fs::read_to_string("trackmap.toml").unwrap_or(String::new()))
+            .expect("invalid trackmap.toml");
 
     let mut spotify = spotify::Spotify::new(&client_id, &client_secret);
 
     match spotify.get_user_playlists(&user_id).await {
         Ok(playlists) => {
+            let max_length = playlists
+                .iter()
+                .map(|playlist| playlist.name.len())
+                .max()
+                .unwrap();
+
             for i in 0..playlists.len() {
-                println!("=== {} ===", playlists[i].name);
+                let total_padding = max_length - playlists[i].name.trim().chars().count() + 2;
+                let left_padding = (total_padding as f64 / 2_f64).ceil() as usize;
+
+                println!(
+                    "===== {:left$}{}{:right$} =====",
+                    "",
+                    playlists[i].name.yellow(),
+                    "",
+                    left = left_padding,
+                    right = total_padding - left_padding
+                );
+
                 let mut tracks_by_name: HashMap<String, Vec<String>> = HashMap::new();
 
                 match spotify.get_tracks(&playlists[i].id, None).await {
@@ -31,17 +87,15 @@ async fn main() {
                                 .entry(track.name.clone())
                                 .or_insert_with(Vec::new);
 
-                            let should_push = match trackmap.tracknames.get(&track.name) {
+                            if match trackmap.tracknames.get(&track.name) {
                                 Some(ids) => !ids.ids.iter().any(|id| entry.contains(id)),
                                 None => true,
-                            };
-
-                            if should_push {
+                            } {
                                 entry.push(track.id.clone());
                             }
                         }
                     }
-                    Err(e) => eprintln!("{}", e),
+                    Err(e) => throw_error(e),
                 }
 
                 let tracks_by_name: HashMap<String, Vec<String>> = tracks_by_name
@@ -51,22 +105,25 @@ async fn main() {
                     .collect();
 
                 for (name, ids) in &tracks_by_name {
-                    print!("{}: ", name);
+                    print!("{}: ", name.cyan());
                     for i in 0..ids.len() {
-                        print!("{}{}", ids[i], if i == ids.len() - 1 { "\n" } else { ", " });
+                        print!(
+                            "{}{}",
+                            ids[i].bright_red(),
+                            if i == ids.len() - 1 { "\n" } else { ", " }
+                        );
                     }
                 }
 
-                println!(
-                    "{}",
-                    if tracks_by_name.len() == 0 {
-                        "no duplicates found\n"
-                    } else {
-                        ""
-                    }
-                );
+                if tracks_by_name.len() == 0 {
+                    println!("{}", "no duplicates found".green());
+                }
+
+                if i < playlists.len() - 1 {
+                    println!();
+                }
             }
         }
-        Err(e) => eprintln!("{}", e),
+        Err(e) => throw_error(e),
     }
 }
