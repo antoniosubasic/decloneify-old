a54978900e26d48f888372a6e1b50a1f57f5b9cb
Antonio Subašić <antonio.subasic.public@gmail.com>
2024-09-13 09:05:53 +0200

    removed unnecessary properties from playlist struct

--- src/playlist.rs
+++ src/playlist.rs
@@ -1,17 +1,9 @@
 use serde::Deserialize;
 
 #[derive(Deserialize)]
-pub struct Tracks {
-    pub href: String,
-    pub total: u64,
-}
-
-#[derive(Deserialize)]
 pub struct Playlist {
-    pub description: String,
     pub id: String,
     pub name: String,
-    pub tracks: Tracks,
 }
 
 #[derive(Deserialize)]
